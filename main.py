095cf195a1ed73e38209cf37efc5db76e7b335d4
ozodbekcommunity <ozodbekcommunity@gmail.com>
2025-11-05 14:27:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from fastapi import *
 from pydantic import BaseModel
 
-api = FastAPI()
+app = FastAPI()
 
 class Item(BaseModel):
     name: str
     description: str
     price: float
 
-@api.get("/")
+@app.get("/")
 async def homepage():
     return {"status": "success"}
 
-@api.get("/product")
+@app.get("/product")
 async def getdata(item: Item = Depends()):
     return {
         "name": item.name,
         "description": item.description,
         "price": item.price
+
     }
